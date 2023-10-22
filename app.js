7118930b122f43f4270d2a14e79a4f26ef08b013
leonelgL20 <leonelgllanos8@gmail.com>
2023-10-21 22:27:02 -0300

    feat: implement express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3030;
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+
+
+app.listen(PORT,() => console.log(`Server running in http://localhost:${PORT}`))
