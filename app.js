3e796c8039447f932e89c73e25ff79159f13cafc
leonelgL20 <leonelgllanos8@gmail.com>
2023-11-22 08:05:24 -0300

    formularios: done.

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const path = require('path');
 const PORT = 3030;
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'/public')))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
-
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
 
 app.listen(PORT,() => console.log(`Server running in http://localhost:${PORT}`))
